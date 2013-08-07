7062f76582b724c116160e6e9ef9a066bd95e7e6
AliekhandroDelRio <alejandro.del.rio.albrechet@gmail.com>
2013-08-07 09:40:11 +0300

    Initial commit

---
+++ src/main/java/com/entities/Track.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+public class Track {
+
+    String title;
+    String singer;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    @Override
+    public String toString() {
+        return "Track [title=" + title + ", singer=" + singer + "]";
+    }
+}
